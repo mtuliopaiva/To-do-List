9f185d53b2e5aedde76fc150b4fd1bc869da91c3
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-07-25 06:07:21 -0300

    Adicionando uma tarefa na minha to-do list

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const minhaTarefa = document.querySelector('input[type=text]'); //meu texto
+var t;
 
 var ul = document.querySelector("#minhaLista");//minha lista existente
 var li = document.createElement("li"); //cria um elemento
 
-li.appendChild(document.createTextNode("Work"));
-ul.appendChild(li); //adiciona um elemento na ul
 
+
+document.querySelector(".adicionar").addEventListener("click", (event) => {
+    t = minhaTarefa.value;
+    
+    li.appendChild(document.createTextNode(t));
+    ul.appendChild(li); //adiciona um elemento na ul
+  });

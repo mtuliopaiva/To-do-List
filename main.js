d9ffe35981930a760cbbc55d4f4c457e3db5eead
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-07-25 06:45:40 -0300

    Adicionando funcionalidade de Add e de Clear

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ const minhaTarefa = document.querySelector('input[type=text]'); //meu texto
 var t;
 
 var ul = document.querySelector("#minhaLista");//minha lista existente
-var li = document.createElement("li"); //cria um elemento
-
 
 
 document.querySelector(".adicionar").addEventListener("click", (event) => {
     t = minhaTarefa.value;
-    
+    var li = document.createElement("li"); //cria um elemento
     li.appendChild(document.createTextNode(t));
     ul.appendChild(li); //adiciona um elemento na ul
+    minhaTarefa.value="";
+  });
+
+  document.querySelector(".limpar").addEventListener("click", () => {
+
+    while(minhaLista != "" ){
+        console.log("deu certo");
+        ul.removeChild(ul.lastChild);
+    }
   });

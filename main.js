c13f40a6d8abe31310c44446040d89c3b2600df8
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-07-26 22:40:24 -0300

    Implementando tarefa realizada - incompleta

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 const minhaTarefa = document.querySelector('input[type=text]'); //meu texto
-var t;
+
+var temp;
+
 
 var ul = document.querySelector("#minhaLista");//minha lista existente
+var li = document.createElement("li");
 
 
 document.querySelector(".adicionar").addEventListener("click", (event) => {
-    t = minhaTarefa.value;
+    temp = minhaTarefa.value;
     var li = document.createElement("li"); //cria um elemento
-    li.appendChild(document.createTextNode(t));
+
+    li.appendChild(document.createTextNode(temp));
     ul.appendChild(li); //adiciona um elemento na ul
     minhaTarefa.value="";
+
   });
 
   document.querySelector(".limpar").addEventListener("click", () => {
-
     while(minhaLista != "" ){
         console.log("deu certo");
         ul.removeChild(ul.lastChild);
     }
   });
+
+  
+  for(var i=0; i<minhaLista.childElementCount; i++){
+      minhaLista.children[i].onclick = function(){
+          console.log("oi");
+      }
+    }
+  //minhaLista.children
